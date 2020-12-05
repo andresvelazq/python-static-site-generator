1cdc8969e200e8b9abd83a07b5fb620601ce3f49
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2020-12-05 14:40:27 -0500

    Site Class

---
+++ ssg/site.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+class Site:
+    # Constructor for class
+    def __init__(self, source, dest):
+        self.source = Path()
+        self.dest = Path()
+
+    # Find root directory
+    def create_dir(self, path):
+        directory = Path(self.dest / relative_to(self.source))
+        directory.mkdir(parents=True, exist_ok=True)
+
+    # Make the destination directory
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        # Recreate all paths
+        for path in self.source.rglob("*"):
+            if os.path.isdir(path):
+                create_dir(self, path)
